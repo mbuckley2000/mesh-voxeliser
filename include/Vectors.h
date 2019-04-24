d619d8a55187bb5e74401376bb0a0cb0242ce1f3
Matt Buckley <mbuckley2000@gmail.com>
2019-04-24 21:20:09 +0100

    Added Vector typedefs

---
+++ include/Vectors.h
@@ -0,0 +1,16 @@
+//
+// Created by matt on 24/04/19.
+//
+
+#ifndef VOXELISE_VECTORS_H
+#define VOXELISE_VECTORS_H
+
+#include <Eigen/Dense>
+
+/*
+	Typedefs
+*/
+typedef Eigen::Vector3f Vec3; //3D float vector
+typedef Eigen::Matrix3f Mat3; //3x3 float matrix
+
+#endif //VOXELISE_VECTORS_H
