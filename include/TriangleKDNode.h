f1ec870a8b3e7f6f3e6e9d4519dd4ded22ca0fae
Matt Buckley <mbuckley2000@gmail.com>
2019-05-05 19:42:43 +0100

    Refactored KD tree code

--- include/TriangleKDNode.h
+++ include/TriangleKDNode.h
@@ -8,39 +8,56 @@
 
 class TriangleKDNode
 {
-  public:
-    Box boundingBox;
-    TriangleKDNode *left;
-    TriangleKDNode *right;
-    std::vector<Triangle *> triangles;
+public:
+  Box boundingBox;
+  TriangleKDNode *left;
+  TriangleKDNode *right;
+  std::vector<Triangle *> triangles;
 
-    /**
-     * Construct and empty leaf node of the tree
-     */
-    TriangleKDNode();
+  /**
+   * Construct and empty leaf node of the tree
+   */
+  TriangleKDNode();
 
-    /**
-     * Construct and balance a tree of the given triangles
-     */
-    TriangleKDNode(std::vector<Triangle *> triangles);
+  /**
+   * Construct and balance a tree of the given triangles
+   */
+  TriangleKDNode(std::vector<Triangle *> triangles, int maxTrianglesPerLeaf);
 
-    /**
-     * Returns true if the given ray intersects with a triangle in the tree
-     * Object*& o will be set to contain a pointer to the intersected triangle
-     * float& t will be set to contain the distance along the ray of the intersection point
-     */
-    bool intersects(Box *box);
+  /**
+   * Returns true if the given ray intersects with a triangle in the tree
+   * Object*& o will be set to contain a pointer to the intersected triangle
+   * float& t will be set to contain the distance along the ray of the intersection point
+   */
+  bool intersects(Box *box);
 
-  private:
-    /**
-     * Builds a balanced KD tree from the triangles
-     */
-    void balance();
+private:
+  /**
+   * Builds a balanced KD tree from the triangles
+   */
+  void balance();
 
-    /**
-     * Expands the bounding box to contain all of the triangles
-     */
-    void expandBoundingBox();
+  /**
+   * Expands the bounding box to contain all of the triangles
+   */
+  void expandBoundingBox();
+
+  /**
+   * Return true if box intersects with any triangles in this node
+   */
+  bool intersectTriangles(Box *box);
+
+  /**
+   * Return true if box intersects with any triangles in this node's children
+   */
+  bool intersectChildren(Box *box);
+
+  /**
+   * Builds the children of this node
+   */
+  void buildChildren();
+
+  int maxTrianglesPerLeaf;
 };
 
 #endif  // VOXELISE_TRIANGLEKDNODE_H
--- src/TriangleKDNode.cpp
+++ src/TriangleKDNode.cpp
@@ -2,142 +2,147 @@
 
 /*
 KD tree for storing HBV for meshes
-Code based on https://blog.frogslayer.com/kd-trees-for-faster-ray-tracing-with-triangles/
 */
 
 // Construct with an empty vector of triangles for a leaf
-TriangleKDNode::TriangleKDNode() : TriangleKDNode(std::vector<Triangle *>()) {}
+TriangleKDNode::TriangleKDNode() : TriangleKDNode(std::vector<Triangle *>(), 10) {}
 
-TriangleKDNode::TriangleKDNode(std::vector<Triangle *> triangles)
+TriangleKDNode::TriangleKDNode(std::vector<Triangle *> triangles, int maxTrianglesPerLeaf)
 {
-    this->left        = nullptr;
-    this->right       = nullptr;
-    this->boundingBox = Box();
-    this->triangles   = triangles;
-    this->balance();
+  this->left                = nullptr;
+  this->right               = nullptr;
+  this->boundingBox         = Box();
+  this->triangles           = triangles;
+  this->maxTrianglesPerLeaf = maxTrianglesPerLeaf;
+  this->balance();
 }
 
 // Comparator for sorting triangles based on their midpoint in the given dimension
 struct CompareDimensions
 {
-    explicit CompareDimensions(int dimension) { this->dimension = dimension; }
+  explicit CompareDimensions(int dimension) { this->dimension = dimension; }
 
-    bool operator()(Triangle *i, Triangle *j) { return (i->getMidpoint()(dimension) < j->getMidpoint()(dimension)); }
+  bool operator()(Triangle *i, Triangle *j) { return (i->getMidpoint()(dimension) < j->getMidpoint()(dimension)); }
 
-    int dimension;
+  int dimension;
 };
 
 // Returns a slice of a vector
-// template <typename T>
 std::vector<Triangle *> sliceVec(int startPos, int endPos, std::vector<Triangle *> vector)
 {
-    std::vector<Triangle *> out;
+  std::vector<Triangle *> out;
 
-    for (int i = startPos; i <= endPos; i++)
-    {
-        out.push_back(vector[i]);
-    }
+  for (int i = startPos; i <= endPos; i++)
+  {
+    out.push_back(vector[i]);
+  }
 
-    return out;
+  return out;
+}
+
+void TriangleKDNode::buildChildren()
+{
+  // Sort the array until middle (median) element and all preceding elements are in the correct place.
+  // Values of all elements preceding median are less than it, and all following median are greater than it.
+  // Much faster than sort() with same eventual outcome (almost 2x speed)
+  const int medianArrayPos = this->triangles.size() / 2;
+  const int largestDim     = this->boundingBox.largestDim();  // Longest dimensions of this node's bounding box
+  std::nth_element(triangles.begin(), triangles.begin() + medianArrayPos, triangles.end(), CompareDimensions(largestDim));
+
+  // Left child will contain all triangles below the median
+  this->left = new TriangleKDNode(sliceVec(0, medianArrayPos, triangles), maxTrianglesPerLeaf);
+
+  if (medianArrayPos + 1 < triangles.size())
+  {
+    // Left child will contain all triangles above the median
+    this->right = new TriangleKDNode(sliceVec(medianArrayPos + 1, triangles.size() - 1, triangles), maxTrianglesPerLeaf);
+  }
 }
 
 void TriangleKDNode::balance()
 {
-    const int maxTrianglesPerLeaf = 10;  // Must be 1 or more
-    static_assert(maxTrianglesPerLeaf >= 1, "Failed at KD tree balancing");
-
-    if (triangles.empty())
-    {
-        return;
-    }
-
-    this->expandBoundingBox();
-
-    if (triangles.size() <= maxTrianglesPerLeaf)
-    {
-        // Finish the tree with empty nodes as leaves
-        this->left  = new TriangleKDNode();
-        this->right = new TriangleKDNode();
-        return;
-    }
-
-    // Sort the array just until middle (median) posision is correct.
-    // All elements preceding median are less than it, and all following median are greater than it.
-    // Much faster than sort() with same eventual outcome (almost 2x speed)
-    const int medianArrayPos = this->triangles.size() / 2;
-    const int largestDim     = this->boundingBox.largestDim();
-    std::nth_element(triangles.begin(), triangles.begin() + medianArrayPos, triangles.end(), CompareDimensions(largestDim));
-
-    // Left tree becomes all photons less than median
-    this->left = new TriangleKDNode(sliceVec(0, medianArrayPos, triangles));
-
-    if (medianArrayPos + 1 < triangles.size())
-    {
-        // Right tree becomes all photons greater than median
-        this->right = new TriangleKDNode(sliceVec(medianArrayPos + 1, triangles.size() - 1, triangles));
-    }
-
+  if (this->triangles.empty())
+  {
+    // This is a leaf node
     return;
+  }
+
+  // Expand to contain all triangles and child nodes
+  this->expandBoundingBox();
+
+  if (this->triangles.size() <= this->maxTrianglesPerLeaf)
+  {
+    // Finish the tree with empty nodes as leaves
+    this->left  = new TriangleKDNode();
+    this->right = new TriangleKDNode();
+    return;
+  }
+
+  this->buildChildren();
 }
 
 void TriangleKDNode::expandBoundingBox()
 {
-    if (this->triangles.size() > 0)
-    {
-        this->boundingBox = triangles[0]->getBoundingBox();
+  if (!this->triangles.empty())
+  {
+    this->boundingBox = triangles[0]->getBoundingBox();
 
-        for (int i = 1; i < triangles.size(); i++)
-        {
-            this->boundingBox.expand(triangles[i]->getBoundingBox());
-        }
+    for (int i = 1; i < triangles.size(); i++)
+    {
+      this->boundingBox.expand(triangles[i]->getBoundingBox());
     }
+  }
+}
+
+bool TriangleKDNode::intersectTriangles(Box *box)
+{
+  for (Triangle *triangle : this->triangles)
+  {
+    // Check for hit
+    if (triangle->intersects(box))
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool TriangleKDNode::intersectChildren(Box *box)
+{
+  bool hitLeft  = false;
+  bool hitRight = false;
+
+  if (!this->left->triangles.empty())
+  {
+    hitLeft = this->left->intersects(box);
+  }
+  if (!this->right->triangles.empty())
+  {
+    hitRight = this->right->intersects(box);
+  }
+
+  return hitLeft || hitRight;
 }
 
 bool TriangleKDNode::intersects(Box *box)
 {
-    // Check to see if the ray intersects the bounding box of the given node
+  // Check to see if the ray intersects the bounding box of the given node
 
-    if (this->boundingBox.intersects(box))
+  if (this->boundingBox.intersects(box))
+  {
+    if (this->left || this->right)
     {
-        bool triangleHit = false;
+      if (this->left->triangles.empty() && this->right->triangles.empty())
+      {
+        // We're at a leaf
+        // Check the triangles for intersections
+        return this->intersectTriangles(box);
+      }
 
-        if (this->left || this->right)
-        {
-            // Intersect with the boxes children until we hit a leaf node
-            if (this->left->triangles.size() > 0 || this->right->triangles.size() > 0)
-            {
-                bool hitLeft  = false;
-                bool hitRight = false;
-
-                if (this->left->triangles.size() > 0)
-                {
-                    hitLeft = this->left->intersects(box);
-                }
-                if (this->right->triangles.size() > 0)
-                {
-                    hitRight = this->right->intersects(box);
-                }
-
-                return hitLeft || hitRight;
-            }
-            else
-            {
-                // We're at a leaf
-                // Check the triangles for intersections
-                for (int i = 0; i < this->triangles.size(); i++)
-                {
-
-                    // Check for hit
-                    if (this->triangles[i]->intersects(box))
-                    {
-                        triangleHit = true;
-                        break;
-                    }
-                }
-
-                return triangleHit;
-            }
-        }
+      // Intersect with the boxes children recursively until we hit a leaf node
+      return this->intersectChildren(box);
     }
-    return false;
+  }
+  return false;
 }
