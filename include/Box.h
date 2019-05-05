8d562057cd15b7486a8dac1e5b56431ae61a6fe8
Matt Buckley <mbuckley2000@gmail.com>
2019-05-05 19:59:17 +0100

    Added box

--- include/Box.h
+++ include/Box.h
@@ -6,34 +6,38 @@
 
 class Box : Intersectable
 {
-  public:
-    Vec3 minimum, maximum;
+public:
+  Vec3 minimum, maximum;
 
-    Box();
-    Box(Vec3 minimum, Vec3 maximum);
+  Box();
+  Box(Vec3 minimum, Vec3 maximum);
 
-    virtual ~Box();
+  virtual ~Box();
 
-    /**
-     * Expands the box to include the given point
-     */
-    void expand(Vec3 v);
+  /**
+   * Expands the box to include the given point
+   */
+  void expand(Vec3 v);
 
-    /**
-     * Expands the box to include the given box
-     */
-    void expand(Box b);
+  /**
+   * Expands the box to include the given box
+   */
+  void expand(Box b);
 
-    /**
-     * Returns the largest dimension (0, 1, 2) of the box
-     */
-    int largestDim();
+  /**
+   * Returns the largest dimension (0, 1, 2) of the box
+   */
+  int largestDim();
 
-    Vec3 getSize();
+  Vec3 getSize();
 
-    Eigen::MatrixXf getVertices();
+  Eigen::MatrixXf getVertices();
 
-    bool intersects(Box *box) override;
+  bool intersects(Box *box) override;
+
+private:
+  bool verticesCalculated = false;
+  Eigen::MatrixXf vertices;
 };
 
 #endif  // VOXELISE_BOX_H
--- src/Box.cpp
+++ src/Box.cpp
@@ -2,106 +2,112 @@
 
 Box::Box(Vec3 minimum, Vec3 maximum)
 {
-    this->minimum = minimum;
-    this->maximum = maximum;
+  this->minimum = minimum;
+  this->maximum = maximum;
 }
 
 Box::Box()
 {
-    this->minimum << INFINITY, INFINITY, INFINITY;
-    this->maximum << -INFINITY, -INFINITY, -INFINITY;
+  this->minimum << INFINITY, INFINITY, INFINITY;
+  this->maximum << -INFINITY, -INFINITY, -INFINITY;
 }
 
 Box::~Box() = default;
 
 void Box::expand(Vec3 v)
 {
-    for (int d = 0; d < 3; d++)
+  for (int d = 0; d < 3; d++)
+  {
+    if (v[d] < this->minimum[d])
     {
-        if (v[d] < this->minimum[d])
-        {
-            this->minimum[d] = v[d];
-        }
-        if (v[d] > this->maximum[d])
-        {
-            this->maximum[d] = v[d];
-        }
+      this->minimum[d] = v[d];
     }
+    if (v[d] > this->maximum[d])
+    {
+      this->maximum[d] = v[d];
+    }
+  }
 }
 
 void Box::expand(Box b)
 {
-    for (int d = 0; d < 3; d++)
+  for (int d = 0; d < 3; d++)
+  {
+    if (b.minimum[d] < this->minimum[d])
     {
-        if (b.minimum[d] < this->minimum[d])
-        {
-            this->minimum[d] = b.minimum[d];
-        }
-        if (b.maximum[d] > this->maximum[d])
-        {
-            this->maximum[d] = b.maximum[d];
-        }
+      this->minimum[d] = b.minimum[d];
     }
+    if (b.maximum[d] > this->maximum[d])
+    {
+      this->maximum[d] = b.maximum[d];
+    }
+  }
 }
 
 int Box::largestDim()
 {
-    float longest = 0;
-    int dim       = 0;
+  float longest = 0;
+  int dim       = 0;
 
-    Vec3 size = this->getSize();
+  Vec3 size = this->getSize();
 
-    for (int d = 0; d < 3; d++)
+  for (int d = 0; d < 3; d++)
+  {
+    const float length = size(d);
+    if (length > longest)
     {
-        const float length = size(d);
-        if (length > longest)
-        {
-            longest = length;
-            dim     = d;
-        }
+      longest = length;
+      dim     = d;
     }
+  }
 
-    return dim;
+  return dim;
 }
 
 Vec3 Box::getSize()
 {
-    return this->maximum - this->minimum;
+  return this->maximum - this->minimum;
 }
 
 bool Box::intersects(Box *box)
 {
-    for (int dim = 0; dim < 3; dim++)
+  for (int dim = 0; dim < 3; dim++)
+  {
+    if (this->minimum(dim) > box->maximum(dim) || box->minimum(dim) > this->maximum(dim))
     {
-        if (this->minimum(dim) > box->maximum(dim) || box->minimum(dim) > this->maximum(dim))
-        {
-            return false;
-        }
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 Eigen::MatrixXf Box::getVertices()
 {
-    Eigen::MatrixXf vertices(3, 8);
+  if (this->verticesCalculated)
+  {
+    return this->vertices;
+  }
 
-    Mat3 minMax   = Mat3();
-    minMax.col(0) = this->minimum;
-    minMax.col(1) = this->maximum;
+  this->vertices = Eigen::MatrixXf(3, 8);
 
-    int counter = 0;
+  Mat3 minMax   = Mat3();
+  minMax.col(0) = this->minimum;
+  minMax.col(1) = this->maximum;
 
-    for (int i = 0; i < 2; i++)
+  int counter = 0;
+
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
     {
-        for (int j = 0; j < 2; j++)
-        {
-            for (int k = 0; k < 2; k++)
-            {
-                vertices.col(counter++) = Vec3(minMax.col(i)(0), minMax.col(j)(1), minMax.col(k)(2));
-            }
-        }
+      for (int k = 0; k < 2; k++)
+      {
+        vertices.col(counter++) = Vec3(minMax.col(i)(0), minMax.col(j)(1), minMax.col(k)(2));
+      }
     }
+  }
 
-    return vertices;
+  this->verticesCalculated = true;
+  return vertices;
 }
